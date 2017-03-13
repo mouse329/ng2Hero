06938048832475b851bd26813aeb64680a15eb88
Tim <Tim.mouse@gmail.com>
2017-03-13 23:19:51 +0800

    git commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 
-
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 
 
 
@@ -72,6 +83,7 @@ import { Hero } from './hero';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  heroes = HEROES;
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { HeorDetailcomponent } from './hero-detail.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeorDetailcomponent } from './hero-detail.component';
 
 @NgModule({
   declarations: [
